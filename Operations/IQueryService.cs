ef878198a397d2a7b828effdc1f4bc1a52f349d2
Om <omberde0@gmail.com>
2026-05-05 16:29:13 +0530

    define project structure and file layout

---
+++ Operations/IQueryService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentDapper.Operations
+{
+    internal interface IQueryService
+    {
+    }
+}
